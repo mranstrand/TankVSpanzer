e55fc502d043d2514a60294a8fa823f72ba4537d
mranstrand <michael@Michaels-MacBook-Pro.local>
2014-10-24 13:04:18 +0200

    bakgrund

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,9 @@
-function start(){
+var c, ctx;
 
+function start() {
+    c = document.getElementById("duk");
+    ctx = c.getContext("2d");
+
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
 }
